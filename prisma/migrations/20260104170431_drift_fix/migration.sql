605355ef17fc2a5ebeaae634b399fb9cc46f07dd
TieuHungHua <linh.lemybkmd@hcmut.edu.vn>
2026-01-05 19:32:16 +0700

    add

--- prisma/migrations/20260104170431_drift_fix/migration.sql
+++ prisma/migrations/20260104170431_drift_fix/migration.sql
@@ -3,37 +3,13 @@ BEGIN;
 CREATE TYPE "UserRole_new" AS ENUM ('student', 'admin', 'lecturer');
 ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT;
 ALTER TABLE "users" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
-<<<<<<< HEAD
-DROP TYPE "UserRole";
-ALTER TYPE "UserRole_new" RENAME TO "UserRole";
-=======
 ALTER TYPE "UserRole" RENAME TO "UserRole_old";
 ALTER TYPE "UserRole_new" RENAME TO "UserRole";
 DROP TYPE "UserRole_old";
->>>>>>> origin/feature/room-booking
 ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'student';
 COMMIT;
 
 -- AlterTable
-<<<<<<< HEAD
-ALTER TABLE "books" ADD COLUMN "cover_image" TEXT;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "password" TEXT NOT NULL;
-ALTER TABLE "users" ADD COLUMN "username" TEXT NOT NULL;
-ALTER TABLE "users" ADD COLUMN "email" TEXT;
-ALTER TABLE "users" ADD COLUMN "phone" TEXT;
-ALTER TABLE "users" ADD COLUMN "student_id" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
-=======
 ALTER TABLE "users" ADD COLUMN     "password" TEXT NOT NULL;
 ALTER TABLE "users" ADD COLUMN     "username" TEXT NOT NULL;
 ALTER TABLE "users" ADD COLUMN     "email" TEXT;
@@ -46,7 +22,6 @@ ALTER TABLE "books" ADD COLUMN     "description" TEXT;
 ALTER TABLE "books" ADD COLUMN     "pages" INTEGER;
 ALTER TABLE "books" ADD COLUMN     "publication_year" INTEGER;
 ALTER TABLE "books" ADD COLUMN     "publisher" TEXT;
->>>>>>> origin/feature/room-booking
 
 -- CreateTable
 CREATE TABLE "comments" (
@@ -69,8 +44,6 @@ CREATE INDEX "comments_book_id_created_at_idx" ON "comments"("book_id", "created
 -- CreateIndex
 CREATE INDEX "comments_user_id_created_at_idx" ON "comments"("user_id", "created_at" DESC);
 
-<<<<<<< HEAD
-=======
 -- CreateIndex
 CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 
@@ -80,7 +53,6 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
 
->>>>>>> origin/feature/room-booking
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
