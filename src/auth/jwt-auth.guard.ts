313f30cc3a82a6e5b52aca555341fc8b84b30323
TieuHungHua <linh.lemybkmd@hcmut.edu.vn>
2026-01-07 23:36:48 +0700

    add faveri book

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') { }
+
+
+// tôi cần commit 
 
 
 
